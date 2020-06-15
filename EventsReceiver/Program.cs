abbe3db68169f4ef7fb625390c6e7362b266617b
Krzysztof Wilczyński <krzysztof_pockit@outlook.com>
2020-06-15 16:58:33 +0200

    Add project files.

---
+++ EventsReceiver/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs.Processor;
+
+namespace EventsReceiver
+{
+    class Program
+    {
+        private const string EhConnectionString = "";
+        private const string EhEntityPath = "customer-events";
+        private const string StorageContainerName = "lease";
+        private const string StorageAccountName = "";
+        private const string StorageAccountKey = "";
+
+        private static readonly string StorageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", StorageAccountName, StorageAccountKey);
+
+        public static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("Registering EventProcessor...");
+
+            var eventProcessorHost = new EventProcessorHost(
+                EhEntityPath,
+                "tests",
+                EhConnectionString,
+                StorageConnectionString,
+                StorageContainerName);
+
+            // Registers the Event Processor Host and starts receiving messages
+            await eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>();
+
+            Console.WriteLine("Receiving. Press ENTER to stop worker.");
+            Console.ReadLine();
+
+            // Disposes of the Event Processor Host
+            await eventProcessorHost.UnregisterEventProcessorAsync();
+        }
+    }
+}
